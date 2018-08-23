13c6c234f5778099c2e7d82ce36d50bb395b163c
arbju <arbjurman@gmail.com>
2018-08-22 20:43:33 -0700

    more

--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+
+# run with:./hello.py -c 4
+
+import sys
 print("Hello Python")
-for i in range(5):
+z = sys.argv[2]
+print(z)
+x = int((sys.argv[2]))
+print("x is ",x)
+for i in range(int(sys.argv[2])):
     print(i)
+
+from tkinter import *
+root = Tk()
+root.geometry('300x300')
+root.title("Tkinter Window")
+text = Label(root, text=z)
+text.pack()
+
+text2 = Label(root, text='Some text')
+text2.pack()
+
+
+root.mainloop()
